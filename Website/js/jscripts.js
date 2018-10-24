f89152155c035641acc6fa445cf6675382e90e83
Orlando Jähde <orlando.jaehde@web.de>
2018-10-24 18:04:09 +0200

    Dropdown sieht gut aus soweit, Checkboxen brauchen noch Anpassung

--- Website/js/jscripts.js
+++ Website/js/jscripts.js
@@ -1,3 +1,6 @@
+/*Dropdown Funktionen für No-Go-Seite*/
+
 function showAllergien() {
   document.getElementById('allergien-dropdown').classList.toggle("show");
+  document.getElementById('essgewohnheiten').classList.toggle("hide");
 };
