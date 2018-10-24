03bde637a443460d7bd47206e6d2f4f3c93dcb3d
Orlando Jähde <orlando.jaehde@web.de>
2018-10-24 18:31:42 +0200

    Checkboxen responsiver, Essgewohnheiten angefangen

--- Website/js/jscripts.js
+++ Website/js/jscripts.js
@@ -1,6 +1,11 @@
 /*Dropdown Funktionen für No-Go-Seite*/
-
+/*Allergien*/
 function showAllergien() {
   document.getElementById('allergien-dropdown').classList.toggle("show");
   document.getElementById('essgewohnheiten').classList.toggle("hide");
+};
+/*Essgewohnheiten*/
+function showEssgewohnheiten() {
+  document.getElementById('essgewohnheiten-dropdown').classList.toggle("show");
+  document.getElementById('allergien').classList.toggle("hide");
 };
