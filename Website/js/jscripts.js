e4709f7c66f46fcebc71d06d2d0282a3cc91f61c
Orlando Jähde <orlando.jaehde@web.de>
2018-10-24 20:30:38 +0200

    Diesdas

--- Website/js/jscripts.js
+++ Website/js/jscripts.js
@@ -1,19 +1,25 @@
 /*Dropdown Funktionen für No-Go-Seite*/
+
 /*Allergien*/
 function showAllergien() {
   document.getElementById('allergien-dropdown').classList.toggle("show");
   /*document.getElementById('essgewohnheiten').classList.toggle("hide");*/
   /*document.getElementById('einzelneZutaten').classList.toggle("hide");*/
 };
+
 /*Essgewohnheiten*/
 function showEssgewohnheiten() {
   document.getElementById('essgewohnheiten-dropdown').classList.toggle("show");
   /*document.getElementById('allergien').classList.toggle("hide");*/
   /*document.getElementById('einzelneZutaten').classList.toggle("hide");*/
 };
+
 /*Einzelne Zutaten*/
 function showZutaten() {
   document.getElementById('zutaten-dropdown').classList.toggle("show");
   /*document.getElementById('allergien').classList.toggle("hide");*/
   /*document.getElementById('essgewohnheiten').classList.toggle("hide");*/
 };
+funcion suchFunktion() {
+  
+}
