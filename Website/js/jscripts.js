b76eb36e7a71322ce9c622c099186f12672890a5
Semjon Geist <1998semi@googlemail.com>
2018-10-24 16:52:55 +0200

    Zusammen Design angepasst..

--- Website/js/jscripts.js
+++ Website/js/jscripts.js
@@ -1,15 +1,3 @@
-// When the user scrolls down 20px from the top of the document, slide down the navbar
-// When the user scrolls to the top of the page, slide up the navbar (50px out of the top view)
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 40 ) {
-    document.getElementById("navbarbg").style.opacity = 0.5;
-  } else {
-    document.getElementById("navbarbg").style.opacity = 0;
-  }
-};
-
 function showAllergien() {
   document.getElementById('allergien-dropdown').classList.toggle("show");
 };
