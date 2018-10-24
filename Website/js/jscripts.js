fd3dfd9bb22a3dfa5cda9928b4e49ae3f1a2b7c6
Orlando Jähde <orlando.jaehde@web.de>
2018-10-24 23:16:21 +0200

    Fusionierung hat geklappt, kleine Designanpassungen

--- Website/js/jscripts.js
+++ Website/js/jscripts.js
@@ -3,22 +3,22 @@
 /*Allergien*/
 function showAllergien() {
   document.getElementById('allergien-dropdown').classList.toggle("show");
-  /*document.getElementById('essgewohnheiten').classList.toggle("hide");*/
-  /*document.getElementById('einzelneZutaten').classList.toggle("hide");*/
+  document.getElementById('essgewohnheiten').classList.toggle("hide");
+  document.getElementById('einzelneZutaten').classList.toggle("hide");
 };
 
 /*Essgewohnheiten*/
 function showEssgewohnheiten() {
   document.getElementById('essgewohnheiten-dropdown').classList.toggle("show");
-  /*document.getElementById('allergien').classList.toggle("hide");*/
-  /*document.getElementById('einzelneZutaten').classList.toggle("hide");*/
+  document.getElementById('allergien').classList.toggle("hide");
+  document.getElementById('einzelneZutaten').classList.toggle("hide");
 };
 
 /*Einzelne Zutaten*/
 function showZutaten() {
   document.getElementById('zutaten-dropdown').classList.toggle("show");
-  /*document.getElementById('allergien').classList.toggle("hide");*/
-  /*document.getElementById('essgewohnheiten').classList.toggle("hide");*/
+  document.getElementById('allergien').classList.toggle("hide");
+  document.getElementById('essgewohnheiten').classList.toggle("hide");
 };
 function suchFunktion() {
   var input, index, zutaten, dropdownInhalt, i;
