88af754d8237390087c6494b3efa93cdd10521b0
Semjon Geist <1998semi@googlemail.com>
2018-11-01 16:19:34 +0100

    Alle Variablen aus der Datenbank übergeben.

--- mysqltest/mysql/index.js
+++ mysqltest/mysql/index.js
@@ -8,34 +8,62 @@ var con = mysql.createConnection({
   database: "what2eat"
 });
 
-var shufflegericht;
+var gerichttitel;
+var idgericht;
+var idzutaten;
+var bezeichnung;
+var menge;
+var mengenangabe;
 
   con.connect(function(err) {
     if (err) throw err;
     con.query("SELECT * FROM gericht", function (err, result, fields) {
       if (err) throw err;
-       shufflegericht = new Array(result.length);
+       gerichttitel = new Array(result.length);
         for (var i = 0; i < result.length; i++) {
-          shufflegericht[i] = result[i].titel;
-          console.log( shufflegericht[i]);
+          gerichttitel[i] = result[i].titel;
+          console.log("Gericht" + i + ": " + gerichttitel[i]);
+        };
+    });
+    con.query("SELECT * FROM zutat", function (err, result, fields) {
+      if (err) throw err;
+       bezeichnung = new Array(result.length);
+        for (var i = 0; i < result.length; i++) {
+          bezeichnung[i] = result[i].bezeichnung;
+          console.log("Zutatbezeichnng" + i +": " + bezeichnung[i])
+        };
+    });
+    con.query("SELECT * FROM rezept", function (err, result, fields) {
+      if (err) throw err;
+       idgericht = new Array(result.length);
+       idzutat = new Array(result.length);
+       menge = new Array(result.length);
+       mengenangabe = new Array(result.length);
+       console.log("Rezept:idgericht,idzutat,menge,mengenangabe");
+        for (var i = 0; i < result.length; i++) {
+          idgericht[i] = result[i].idgericht;
+          idzutat[i] = result[i].idzutat;
+          menge[i] = result[i].menge;
+          mengenangabe[i] = result[i].mengenangabe;
+          console.log("Rezept:" + idgericht[i] + "," + idzutat[i] + "," + menge[i] + "," + mengenangabe[i]);
         };
     });
   });
 
   router.get('/regis', (req, res) => {
       console.log('Request for regis page recieved');
-      res.render('regis', {shufflegericht});
+      res.render('regis', {gerichttitel, idgericht, idzutat, bezeichnung, menge, mengenangabe});
   })
 
 
   router.get('/shuffle', (req, res) => {
         console.log('Request for nogos page recieved');
-        res.render('shuffle', {shufflegericht});
+        res.render('shuffle', {gerichttitel, idgericht, idzutat, bezeichnung, menge, mengenangabe});
     })
 
   router.get('/login', (req, res) => {
         console.log('Request for nogos page recieved');
-        res.render('login', {shufflegericht});
+        res.render('login', {gerichttitel, idgericht, idzutat, bezeichnung, menge, mengenangabe});
   })
 
   module.exports = router;
