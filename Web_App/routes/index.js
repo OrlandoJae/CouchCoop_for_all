2d5ef2aa0bd43738971afffb36bf36098b65e0be
Semjon Geist <1998semi@googlemail.com>
2018-11-03 21:14:43 +0100

    -

--- Web_App/routes/index.js
+++ Web_App/routes/index.js
@@ -1,5 +1,70 @@
 var express = require("express");
 const router = express.Router();
+var mysql = require('mysql');
+var con = mysql.createConnection({
+  host: "sql7.freemysqlhosting.net",
+  user: "sql7263848",
+  password: 'jPJhq34Qr5',
+  database: "sql7263848",
+});
+
+var gerichttitel;
+var idgericht;
+var idzutaten;
+var bezeichnung;
+var menge;
+var mengenangabe;
+
+  con.connect(function(err) {
+    if (err) throw err;
+    con.query("SELECT * FROM gericht", function (err, result, fields) {
+      if (err) throw err;
+       gerichttitel = new Array(result.length);
+        for (var i = 0; i < result.length; i++) {
+          gerichttitel[i] = result[i].titel;
+          console.log("Gericht" + i + ": " + gerichttitel[i]);
+        };
+    });
+    con.query("SELECT * FROM zutat", function (err, result, fields) {
+      if (err) throw err;
+       bezeichnung = new Array(result.length);
+        for (var i = 0; i < result.length; i++) {
+          bezeichnung[i] = result[i].bezeichnung;
+          console.log("Zutatbezeichnng" + i +": " + bezeichnung[i])
+        };
+    });
+    con.query("SELECT * FROM rezept", function (err, result, fields) {
+      if (err) throw err;
+       idgericht = new Array(result.length);
+       idzutat = new Array(result.length);
+       menge = new Array(result.length);
+       mengenangabe = new Array(result.length);
+       console.log("Rezept:idgericht,idzutat,menge,mengenangabe");
+        for (var i = 0; i < result.length; i++) {
+          idgericht[i] = result[i].idgericht;
+          idzutat[i] = result[i].idzutat;
+          menge[i] = result[i].menge;
+          mengenangabe[i] = result[i].mengenangabe;
+          console.log("Rezept:" + idgericht[i] + "," + idzutat[i] + "," + menge[i] + "," + mengenangabe[i]);
+        };
+    });
+  });
+
+  router.get('/regis', (req, res) => {
+      console.log('Request for regis page recieved');
+      res.render('regis', {gerichttitel, idgericht, idzutat, bezeichnung, menge, mengenangabe});
+  })
+
+
+  router.get('/shuffle', (req, res) => {
+        console.log('Request for nogos page recieved');
+        res.render('shuffle', {gerichttitel, idgericht, idzutat, bezeichnung, menge, mengenangabe});
+    })
+
+  router.get('/login', (req, res) => {
+        console.log('Request for nogos page recieved');
+        res.render('login', {gerichttitel, idgericht, idzutat, bezeichnung, menge, mengenangabe});
+  })
 
 
   router.get('/', (req, res) => {
@@ -10,7 +75,7 @@ const router = express.Router();
 
   router.get('/nogos', (req, res) => {
       console.log('Request for nogos page recieved');
-      res.render('nogos');
+      res.render('nogos', {gerichttitel, idgericht, idzutat, bezeichnung, menge, mengenangabe});
   })
 
 
